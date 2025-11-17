7165da541308e08f1b892e23e2cab20f3f5cae9b
Reno Sebastian Nugraha <pssttt1@gmail.com>
2025-11-17 09:47:02 +0700

    perbaikan struktur folder

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-// routes/web.php
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/tes-pohon-organisasi', function () {
+    return view('organisasi');
+});
+
 Route::view('/login', 'auth.login')->name('login');
 Route::get('/', fn() => redirect()->route('login'));
 Route::view('/staf/dashboard', 'staf.dashboard')

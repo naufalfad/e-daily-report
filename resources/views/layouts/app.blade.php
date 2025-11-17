38d641cd5031bcae945eb89abf509533af85d028
Reno Sebastian Nugraha <pssttt1@gmail.com>
2025-11-17 10:46:17 +0700

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? 'E-Daily Report' }}</title>
-    @vite(['resources/js/app.js'])
-    <link rel="stylesheet" href="{{ asset('build/tailwind.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
